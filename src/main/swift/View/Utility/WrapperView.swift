d8061fd80224117955baba1fcf11fcc99ac9bd14
Asia Serrano <asiamserrano@gmail.com>
2025-08-07 17:58:36 -0400

    new wrapper view

---
+++ src/main/swift/View/Utility/WrapperView.swift
@@ -0,0 +1,18 @@
+//
+//  WrapperView.swift
+//  autosave
+//
+//  Created by Asia Michelle Serrano on 8/7/25.
+//
+
+import SwiftUI
+
+struct WrapperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WrapperView()
+}
