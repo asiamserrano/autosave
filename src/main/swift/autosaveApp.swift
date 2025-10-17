667fa99651d7b34d86b08f771d68734f5325da1b
Asia Serrano <asiamserrano@gmail.com>
2025-10-17 10:47:27 -0400

    adding resources

--- src/main/swift/autosaveApp.swift
+++ src/main/swift/autosaveApp.swift
@@ -16,60 +16,18 @@ struct autosaveApp: App {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: .defaultValue, inMemory: false, isAutosaveEnabled: false, isUndoEnabled: true)
         
     }
     
 }
 
 fileprivate struct ContentView: View {
-    
-    @Environment(\.modelContext) private var modelContext
-    
-    @State var status: GameStatusEnum = .defaultValue
-    @State var menu: MenuEnum = .defaultValue
-    
+        
+
     var body: some View {
         NavigationStack {
-            Group {
-                switch self.menu {
-                case .library, .wishlist:
-                    GamesListView(self.status)
-                case .properties:
-                    Text("TBD")
-                }
-            }
-            .navigationTitle(self.menu.rawValue)
-            .onChange(of: self.menu) {
-                switch self.menu {
-                case .library, .wishlist:
-                    let id: String = self.menu.id
-                    self.status = .init(id)
-                case .properties:
-                    break
-                }
-            }
-            .toolbar {
-                
-                ToolbarItem(placement: .topBarLeading, content: {
-                    Menu(content: {
-                        Picker(.defaultValue, selection: $menu, content: {
-                            ForEach(MenuEnum.cases) { menu in
-                                HStack {
-                                    Text(menu.rawValue)
-                                    Image(menu.icon)
-                                }
-                                .tag(menu)
-                            }
-                        }).pickerStyle(.automatic)
-                    }, label: {
-                        IconView(.line_3_horizontal)
-                    })
-                })
-                
-            }
+            Text("Hello, World!")
         }
-        
     }
     
 }
@@ -77,22 +35,6 @@ fileprivate struct ContentView: View {
 
 #Preview {
     
-    let previewModelContainer: ModelContainer = {
-        
-        let container: ModelContainer = .preview
-        
-        container.mainContext.autosaveEnabled = false
-        container.mainContext.undoManager = .init()
-        
-        for _ in 0..<20 {
-            let snapshot: GameSnapshot = .random
-            container.mainContext.save(snapshot)
-        }
-        
-        return container
-        
-    }()
+    ContentView()
     
-    return ContentView()
-        .modelContainer(previewModelContainer)
 }
